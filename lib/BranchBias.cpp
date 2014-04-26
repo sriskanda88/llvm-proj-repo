5c52c8fbcd46f4210f755c595425c48269abf134
Andreas Prodromou <prodromou.andreas@gmail.com>
2014-04-26 22:16:14 +0300

    Fixed the BranchBias pass. An intermediate basic block is created and placed between the actual branch and its corresponding taken block

--- lib/BranchBias.cpp
+++ lib/BranchBias.cpp
@@ -22,12 +22,15 @@ namespace {
         Function *branchFound; //Keeps the total counter
         Function *branchTaken; // Keeps a counter of Taken branches
         Function *printFunction; // Prints Statistics
+        Function *currentFunction; // Prints Statistics
+        BasicBlock *lastBlock;
 
         StringRef functionName; // The name of the function being currently analyzed
 
         BranchBias() : ModulePass (ID) {}
 
         virtual bool runOnModule (Module &M) {
+            lastBlock = NULL;
 
             // Initialize the pointers to the functions
             branchTaken = cast<Function> (M.getOrInsertFunction("_Z11branchTakenPc", 
@@ -50,19 +53,13 @@ namespace {
             {
                 // Keep the function name in global variable
                 functionName = (*F).getName();
+                currentFunction = F;
+                if (F->getName() == "main") lastBlock = &(F->back());
                 
                 // Loop over all Basic Blocks
                 for(Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
                 {
                     BranchBias::runOnBasicBlock(BB);
-                }
-
-                // Add the print helper function before the program's end 
-                // (last Basic Block of Main function)
-                if ((*F).getName() == "main") {
-                    Function::iterator lastBlock = F->end();
-                    lastBlock--;
-                    BranchBias::addPrintCall(lastBlock);
                 }
             }
             return false;
@@ -75,6 +72,13 @@ namespace {
 
             for(BasicBlock::iterator BI = BB->begin(), BE = BB->end(); BI != BE; ++BI)
             {
+                //If the function is main and we reached the return instruction
+                //add a call to the print statistics function
+                if ((isa<ReturnInst>(BI)) && (currentFunction->getName() == "main")) {
+                    BranchBias::addPrintCall(BB);
+                    
+                }
+
                 //Only operate on Branch Instructions
                 if(isa<BranchInst>(&(*BI)) ) {
                     BranchInst *CI = dyn_cast<BranchInst>(BI);
@@ -95,15 +99,14 @@ namespace {
                     // Successor 0 is the "Taken" basic block
 
                     // Get the "Taken" Basic Block and move to its first instruction
-                    BasicBlock *block = CI->getSuccessor(0);
-                    BasicBlock::iterator takenIns = block->getFirstInsertionPt();
-                    //takenIns++;
+                    BasicBlock *takenBlock = CI->getSuccessor(0);
+                    
+                    //Create a new intermediate block
+                    BasicBlock *newBlock = BasicBlock::Create(getGlobalContext(), "newBlock", currentFunction);
 
-                    // Create an IRBuilder instance and set the insert point 
-                    // to the beginning of the "Taken" basic block
-                    IRBuilder<> builder(block);
-                    builder.SetInsertPoint(takenIns);
-
+                    // Start building the new block
+                    IRBuilder<> builder(newBlock);
+                    
                     // Create a globally visible string with the function name and 
                     // call the branchTaken function with it
 
@@ -113,6 +116,17 @@ namespace {
                         isFirst = false;
                     }
                     builder.CreateCall (branchTaken, myStr);
+                    // Add unconditional jump to the "taken" block
+                    builder.CreateBr (takenBlock);
+
+                    // Fix the PHI node if it has one
+                    CI->setSuccessor(0, newBlock); 
+                    Instruction *takenIns = takenBlock->begin();
+                    if (isa<PHINode>(takenIns)) {
+                        PHINode *phi = cast<PHINode>(takenIns);
+                        int bb_index = phi->getBasicBlockIndex(BB);
+                        phi->setIncomingBlock (bb_index, newBlock);
+                    }
 
                     /* CALL TO BRANCH TAKEN COMPLETED */
 
@@ -134,8 +148,7 @@ namespace {
             // Create an IRBuilder instance and set the insert point 
             // to the very last instruction of main
             IRBuilder<> builder(BB);
-            BasicBlock::iterator lastIns = BB->end();
-            lastIns--;
+            BasicBlock::iterator lastIns = BB->back();
             builder.SetInsertPoint(lastIns);
 
             // Create a call to the printFunction with no arguments
